91e70ed7c2aed90d99923c67dbf94f732fca00b6
jaddotwuzhere <jaddotplayz@gmail.com>
2025-11-30 18:30:51 -0500

    Added colored pixels

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from camera import Camera
-from objects.sphere import Sphere
 from render import *
 from util import *
 
 o = create_vector(0, 0, -3)
-S = Sphere(o, 0.5)
+S = Sphere(o, 1.1)
 cam = Camera(ORIGIN, DEFWIN_WIDTH, DEFWIN_HEIGHT, 1)
 
 pixels = renderGradient(cam, S, DEFWIN_WIDTH, DEFWIN_HEIGHT)
--- render.py
+++ render.py
@@ -1,6 +1,6 @@
 # draws the window thing
 from camera import *
-from geometry import intersectPt
+from geometry import intersectPt, normalVec
 from objects.sphere import Sphere
 from util import *
 
@@ -10,12 +10,12 @@ def renderGradient(camera, sphere, width, height):
     pixelBuf = [[0 for _ in range(width)] for _ in range(height)]
     for i in range(height):
         for j in range(width):
-            R = camera.genRay(j, i)
-            P = intersectPt(R, sphere)
+            ray = camera.genRay(j, i)
+            P = intersectPt(ray, sphere)
             if P is SENTINEL:
                 pixelBuf[i][j] = SKYBOX
             else:
-                pixelBuf[i][j] = (0, 0, 255)
+                pixelBuf[i][j] = colorSurfNorm(ray, sphere)
     return pixelBuf
 
 def writePPM(filename, width, height, pixels):
@@ -27,3 +27,13 @@ def writePPM(filename, width, height, pixels):
             for j in range(width):
                 r, g, b = pixels[i][j]
                 f.write(f"{r} {g} {b}\n")
+
+def colorHelper(n):
+    return (n + 1) / 2
+
+def colorSurfNorm(ray, sphere):
+    N = normalVec(ray, sphere)
+    c_r = int(colorHelper(N[0]) * 255)
+    c_g = int(colorHelper(N[1]) * 255)
+    c_b = int(colorHelper(N[2]) * 255)
+    return (c_r, c_g, c_b)
