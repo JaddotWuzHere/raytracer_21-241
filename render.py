94fa86de537915989176e0da3a068ad98bbb1a40
jaddotwuzhere <jaddotplayz@gmail.com>
2025-11-30 20:01:11 -0500

    Added ambient lighting

--- render.py
+++ render.py
@@ -4,7 +4,9 @@ from util import *
 
 SKYBOX = (255, 255, 255)
 LIGHT_POS = create_vector(2, 2, 0)
-SHADOW_STR = 0.85 # [0, 1]
+
+AMBIENT = 0.25
+DIFFUSE = 1.0
 
 def renderGradient(camera, sphere, width, height):
     pixelBuf = [[0 for _ in range(width)] for _ in range(height)]
@@ -34,11 +36,24 @@ def lambertianShade(ray, sphere): # ret: (r, g, b)
     P = intersectPt(ray, sphere)
     N = normalize(P - sphere.center)
     L = normalize(LIGHT_POS - P)
-    diff = max((1 - SHADOW_STR), dot(N, L))
-    c_r = int(colorSurfNorm(ray, sphere)[0] * diff)
-    c_b = int(colorSurfNorm(ray, sphere)[1] * diff)
-    c_g = int(colorSurfNorm(ray, sphere)[2] * diff)
-    return (c_r, c_g, c_b)
+    diff = max(0, dot(N, L))
+    brightness = AMBIENT + DIFFUSE * diff
+    c_r = colorSurfNorm(ray, sphere)[0]
+    c_g = colorSurfNorm(ray, sphere)[1]
+    c_b = colorSurfNorm(ray, sphere)[2]
+    C_r = int(c_r * brightness)
+    C_g = int(c_g * brightness)
+    C_b = int(c_b * brightness)
+
+    # clamping
+    if C_r > 255: C_r = 255
+    elif C_r < 0: C_r = 0
+    if C_g > 255: C_g = 255
+    elif C_g < 0: C_g = 0
+    if C_b > 255: C_b = 255
+    elif C_b < 0: C_b = 0
+
+    return (C_r, C_g, C_b)
 
 # generate file
 def writePPM(filename, width, height, pixels):
@@ -49,4 +64,4 @@ def writePPM(filename, width, height, pixels):
         for i in range(height):
             for j in range(width):
                 r, g, b = pixels[i][j]
-                f.write(f"{r} {g} {b}\n")
+                f.write(f"{r} {g} {b} \n")
