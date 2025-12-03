3ff42f012d846ab1f94228771ed92ef5683bf1ee
jaddotwuzhere <jaddotplayz@gmail.com>
2025-12-03 15:36:15 -0500

    Upgraded camera functionality

--- camera.py
+++ camera.py
@@ -1,13 +1,29 @@
+import math
+
 from objects.ray import Ray
 from util import *
 
+WORLD_UP = create_vector(0, 1, 0)
+
+def rotateAroundAxis(v, axis, angle):
+    ax = normalize(axis)
+    vParallel = ax * dot(ax, v)
+    vPerp = v - vParallel
+
+    vRot = vPerp * math.cos(angle) + cross(ax, vPerp) * math.sin(angle)
+    return vParallel + vRot
+
 class Camera:
-    def __init__(self, origin, imgWidth, imgHeight, fov):
-        self.origin = origin # vector3d
+    def __init__(self, pos, lookAt, imgWidth, imgHeight, focalLength):
+        self.pos = pos # vector3d
         self.imgWidth = imgWidth # int
         self.imgHeight = imgHeight # int
-        self.fov = fov # float
+        self.focalLength = focalLength # float
         self.aspectRatio = imgWidth / imgHeight #float
+
+        self.forward = normalize(lookAt - pos)
+        self.right = normalize(cross(self.forward, WORLD_UP))
+        self.up = normalize(cross(self.right, self.forward))
 
     def genRay(self, i, j): # ret: vector3d
         # clamping [0, 1]%
@@ -18,9 +34,36 @@ class Camera:
         x = (2 * u - 1) * self.aspectRatio
         y = 1 - 2 * v
 
-        P = create_vector(x, y, -self.fov)
-        rawD = P - self.origin
-        D = normalize(rawD)
+        forwardComponent = self.forward * self.focalLength
+        rightComponent = self.right * x
+        upComponent = self.up * y
+        direction = forwardComponent + rightComponent + upComponent
+        D = normalize(direction)
 
-        R = Ray(self.origin, D)
+        R = Ray(self.pos, D)
         return R
+
+    def moveDepth(self, amt):
+        self.pos += self.forward * amt
+
+    def moveHorizontal(self, amt):
+        self.pos += self.right * amt
+
+    def moveVertical(self, amt):
+        self.pos += self.up * amt
+
+    def yaw(self, angle):
+        axis = WORLD_UP
+        newForward = rotateAroundAxis(self.forward, axis, angle)
+        newRight = rotateAroundAxis(self.right, axis, angle)
+        self.forward = normalize(newForward)
+        self.right = normalize(newRight)
+        self.up = normalize(cross(newRight, newForward))
+
+    def pitch(self, angle):
+        axis = self.right
+        newForward = rotateAroundAxis(self.forward, axis, angle)
+        newUp = rotateAroundAxis(self.up, axis, angle)
+        self.forward = normalize(newForward)
+        self.right = normalize(cross(newForward, newUp))
+        self.up = normalize(cross(self.right, newForward))
--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from objects.sphere import Sphere
 from render import *
 from util import *
 
-# BUG PLEASE FIX AHHHH
 S1 = Sphere(create_vector(0, 0, -3), 1)
 S2 = Sphere(create_vector(-1, 0, -2), 1)
 S3 = Sphere(create_vector(-2, 0, -1), 1)
 S = [S1, S2, S3]
 
-cam = Camera(ORIGIN, DEFWIN_WIDTH, DEFWIN_HEIGHT, 1)
+cam = Camera(ORIGIN, create_vector(0, 0, -1), DEFWIN_WIDTH, DEFWIN_HEIGHT, 1)
 
 pixels = renderGradient(cam, S, DEFWIN_WIDTH, DEFWIN_HEIGHT)
 writePPM("output", DEFWIN_WIDTH, DEFWIN_HEIGHT, pixels)
