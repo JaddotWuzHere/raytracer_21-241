6c3c2cd17a7393d6508be21a25eef8a051c9b693
jaddotwuzhere <jaddotplayz@gmail.com>
2025-12-03 19:05:06 -0500

    Implemented shitty mouse look mechanic

--- main.py
+++ main.py
@@ -10,48 +10,57 @@ from util import *
 
 # camera speed, def = 3.0
 SPEED = 3.0
+# mouse sensitivity, def = 0.003
+SENS = 0.003
+# mouse center
+CENTER_X, CENTER_Y = DEFWIN_WIDTH // 2, DEFWIN_HEIGHT // 2
 
 def camInput(camera, seconds):
     keys = pygame.key.get_pressed()
-    moved = False
 
     if keys[pygame.K_w]:
         camera.moveDepth(+SPEED * seconds)
-        moved = True
     if keys[pygame.K_s]:
         camera.moveDepth(-SPEED * seconds)
-        moved = True
 
     if keys[pygame.K_d]:
         camera.moveHorizontal(+SPEED * seconds)
-        moved = True
     if keys[pygame.K_a]:
         camera.moveHorizontal(-SPEED * seconds)
-        moved = True
 
     if keys[pygame.K_SPACE]:
         camera.moveVertical(+SPEED * seconds)
-        moved = True
     if keys[pygame.K_LSHIFT]:
         camera.moveVertical(-SPEED * seconds)
-        moved = True
 
     # disable "vectoring"
     # if length(move) > 0.0:
     #     move = normalize(move)
     #     camera.pos += move * SPEED * seconds
 
-    return moved
+def mouseLook(camera):
+    dx, dy = pygame.mouse.get_rel()
+
+    camera.yaw(-dx * SENS)
+    camera.pitch(-dy * SENS)
+
+    pygame.mouse.set_pos(CENTER_X, CENTER_Y)
 
 def buildScene():
-    s1 = Sphere(create_vector(0, 0, -3), 1)
-    return Scene([s1])
+    s1 = Sphere(create_vector(-2, 0, -1), 1)
+    s2 = Sphere(create_vector(-1, 0, -2), 1)
+    return Scene([s1, s2])
 
 def buildCamera():
     return Camera(ORIGIN, create_vector(0, 0, -1), DEFWIN_WIDTH, DEFWIN_HEIGHT, 1)
 
 def main():
     pygame.init()
+    pygame.mouse.set_visible(False)
+    pygame.event.set_grab(True)
+    pygame.mouse.set_pos(CENTER_X, CENTER_Y)
+    pygame.mouse.get_rel()
+
     screen = pygame.display.set_mode((DEFWIN_WIDTH, DEFWIN_HEIGHT))
     clock = pygame.time.Clock()
 
@@ -69,6 +78,7 @@ def main():
                 running = False
 
         camInput(camera, seconds)
+        mouseLook(camera)
 
         pixels = renderFrame(camera, scene, DEFWIN_WIDTH, DEFWIN_HEIGHT)
         byteBuf = writeBytes(pixels, DEFWIN_WIDTH, DEFWIN_HEIGHT)
